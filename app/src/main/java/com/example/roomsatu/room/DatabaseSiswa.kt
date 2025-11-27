be39d54e8c1e94e9b3084c9d24b076720d660cae
aldys002 <igidiaaldys@gmail.com>
2025-11-27 09:57:39 +0700

    merubah myroomsatu menjadi roomsatu

--- app/src/main/java/com/example/roomsatu/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomsatu/room/DatabaseSiswa.kt
@@ -1,9 +1,10 @@
-package com.example.myroomsatu.room
+package com.example.roomsatu.room
 
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.myroomsatu.room.SiswaDao
 import com.example.roomsatu.room.Siswa
 
 
