e7d9aa341cf36f4203992d3df2115f81dea690c7
aldys002 <igidiaaldys@gmail.com>
2025-11-27 13:36:48 +0700

    Menambahkan objek DestinasiHome sebagai konfigurasi rute utama dengan title resource aplikasi

---
+++ app/src/main/java/com/example/roomsatu/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.roomsatu.view.route
+
+import androidx.compose.ui.res.stringResource
+import com.example.roomsatu.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = (R.string.app_name)
+}
