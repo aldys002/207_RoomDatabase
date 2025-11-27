8c6bd4d3c777a90d9c5697b0f5e1802f26ed4e65
aldys002 <igidiaaldys@gmail.com>
2025-11-27 09:47:32 +0700

    Menambahkan dependency container untuk repository siswa

---
+++ app/src/main/java/com/example/roomsatu/repositori/ContainerApp.kt
@@ -0,0 +1,27 @@
+package com.example.roomsatu.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.myroomsatu.room.DatabaseSiswa
+
+interface ContainerApp{
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+        ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            siswaDao = DatabaseSiswa.getDatabase(context).siswaDao()
+        )
+    }
+}
+
+class AplikasiSiswa : Application() {
+    //apcontainer instance digunakan oleh kelas" lainnya untuk menaympaikan deoendensi
+
+    lateinit var container : ContainerApp
+
+    override fun onCreate() {
+    }
+}
