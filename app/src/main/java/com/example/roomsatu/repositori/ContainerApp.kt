6de63561219ac47e6c928210d704a1402b3ea5c2
aldys002 <igidiaaldys@gmail.com>
2025-11-27 10:33:52 +0700

    Menambahkan inisialisasi container pada AplikasiSiswa

--- app/src/main/java/com/example/roomsatu/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomsatu/repositori/ContainerApp.kt
@@ -23,5 +23,7 @@ class AplikasiSiswa : Application() {
     lateinit var container : ContainerApp
 
     override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
     }
 }
