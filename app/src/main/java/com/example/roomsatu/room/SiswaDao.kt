47518da55ead33e182055fdb8bd873f267e48918
aldys002 <igidiaaldys@gmail.com>
2025-11-27 09:59:25 +0700

    fix tblsiswa jadi siswa

--- app/src/main/java/com/example/roomsatu/room/SiswaDao.kt
+++ app/src/main/java/com/example/roomsatu/room/SiswaDao.kt
@@ -1,10 +1,9 @@
-package com.example.myroomsatu.room
+package com.example.roomsatu.room
 
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.example.roomsatu.room.Siswa
 import kotlinx.coroutines.flow.Flow
 
 @Dao
