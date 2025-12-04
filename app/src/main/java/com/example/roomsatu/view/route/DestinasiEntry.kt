c2b29d0bdcd3f2abacd0caf6cd46fd2e9e32004a
aldys002 <igidiaaldys@gmail.com>
2025-12-04 14:10:29 +0700

    menambah destinasi entry siswa

--- app/src/main/java/com/example/roomsatu/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/roomsatu/view/route/DestinasiEntry.kt
@@ -2,7 +2,7 @@ package com.example.roomsatu.view.route
 
 import com.example.roomsatu.R
 
-object DestinasIEntry : DestinasiNavigasi {
+object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = (R.string.entry_siswa)
 }
