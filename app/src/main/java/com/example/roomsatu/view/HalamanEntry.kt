ae36710ec62463d1714a45316a585b5c008457c5
aldys002 <igidiaaldys@gmail.com>
2025-12-04 09:26:47 +0700

    pindah file dr flder uicontroller ke folder view

--- app/src/main/java/com/example/roomsatu/view/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsatu/view/HalamanEntry.kt
@@ -1,4 +1,4 @@
-package com.example.roomsatu.view.uicontroller
+package com.example.roomsatu.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
