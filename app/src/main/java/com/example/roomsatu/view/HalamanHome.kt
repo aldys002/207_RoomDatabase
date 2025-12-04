9b505804132254d6846d4be557e81cb05f6f491b
aldys002 <igidiaaldys@gmail.com>
2025-12-04 09:27:07 +0700

    memindahkan file dr folder uicontroller ke folder view

--- app/src/main/java/com/example/roomsatu/view/HalamanHome.kt
+++ app/src/main/java/com/example/roomsatu/view/HalamanHome.kt
@@ -1,5 +1,6 @@
-package com.example.roomsatu.view.uicontroller
+package com.example.roomsatu.view
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -43,6 +44,7 @@ import com.example.roomsatu.view.viewmodel.provider.PenyediaViewModel
 @Composable
 fun HomeScreen(
     navigateToItemEntry: () -> Unit,
+    navigateToItemDetail:(Int) -> Unit,
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
@@ -74,6 +76,7 @@ fun HomeScreen(
         val uiStateSiswa by viewModel.homeUiState.collectAsState()
         BodyHome(
             itemSiswa = uiStateSiswa.listSiswa,
+            onSiswaClick = navigateToItemDetail,
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxSize()
@@ -84,6 +87,7 @@ fun HomeScreen(
 @Composable
 fun BodyHome(
     itemSiswa: List<Siswa>,
+    onSiswaClick: (Int) -> Unit,
     modifier: Modifier = Modifier){
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -98,6 +102,7 @@ fun BodyHome(
         } else {
             ListSiswa(
                 itemSiswa = itemSiswa,
+                onSiswaClick = {onSiswaClick(it.id)},
                 modifier = Modifier.padding(horizontal = dimensionResource(R.dimen.padding_small))
             )
         }
@@ -107,6 +112,8 @@ fun BodyHome(
 @Composable
 fun ListSiswa(
     itemSiswa: List<Siswa>,
+    //edit1 : tambahkan parameter onSubmitClick
+    onSiswaClick : (Siswa) -> Unit,
     modifier: Modifier= Modifier
 ){
     LazyColumn (modifier=Modifier){
@@ -115,7 +122,8 @@ fun ListSiswa(
                 siswa = person,
                 modifier = Modifier
                     .padding(dimensionResource(R.dimen.padding_small))
-            )
+                //edit2 : tambahkan modifikasi clickable
+                    .clickable {onSiswaClick(person)})
         }
     }
 }
